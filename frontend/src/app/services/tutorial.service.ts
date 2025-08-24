6c2021a9c10d323e3f6a3b35914f097ea92b2735
Navinder Gill <nnavigill784@gmail.com>
2025-08-24 18:59:06 +0530

    adding the base url

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
-
-const baseUrl = "http://backend:8080/tutorials";
-
+const baseUrl = "http://backend:8080/api/tutorials";
 
 @Injectable({
   providedIn: 'root'
